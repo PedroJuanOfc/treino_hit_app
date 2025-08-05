09ea711ef056f9e5d876efe397f5a7ad1a7940b4
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-08-04 22:13:02 -0300

    chore: definir HomeScreen como tela inicial no main.dart

---
+++ lib/models/exercise.dart
@@ -0,0 +1,11 @@
+class Exercise {
+  final String name;
+  final int durationSeconds;
+  final int restSeconds;
+
+  Exercise({
+    required this.name,
+    required this.durationSeconds,
+    required this.restSeconds,
+  });
+}
